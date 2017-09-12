d7b612511b7e539df71713ab1618f1bad2378984
Jason Hsu <hsujasonf@gmail.com>
2017-09-12 13:59:10 -0700

    cleaned up tests

--- src/test/MenuItemComponent.test.js
+++ src/test/MenuItemComponent.test.js
@@ -37,19 +37,22 @@ const menuItems = [
 	];
 
 describe('Testing MenuItemComponent',()=>{
-	const shallowWrapper= shallow(<MenuItemComponent item={item} onAddItem={onAddItem}/>)
+	
 
 it('should render the menu item name', ()=>{
+	const shallowWrapper= shallow(<MenuItemComponent item={item} onAddItem={onAddItem}/>)
 	const itemNameWrapper = shallowWrapper.find('.card-title')
 	assert.equal(itemNameWrapper.length,1)
 	
 })
 it('should render the menu price', ()=>{
+	const shallowWrapper= shallow(<MenuItemComponent item={item} onAddItem={onAddItem}/>)
 	let itemPriceWrapper = shallowWrapper.find('.card-content').children('p')
 	assert.equal(price.length,1)
 	
 })
 it('should test add Item',()=>{
+const shallowWrapper= shallow(<MenuItemComponent item={item} onAddItem={onAddItem}/>)
 const onAddItem = jest.fn()
 mount(<MenuItemComponent onAddItem={onAddItem} item={item}/>).find('.menuChoice').simulate('click')
 expect(onAddItem).toHaveBeenCalled()
@@ -58,7 +61,8 @@ expect(onAddItem).toHaveBeenCalled()
 })
 
 it('renders correctly',()=>{
-
+const shallowWrapper= shallow(<MenuItemComponent item={item} onAddItem={onAddItem}/>)
+const item = { name: 'Some Item A', price: 9.99, imagePath: '//via.placeholder.com/300x200' }
 const image = renderer.create(<img src={item.imagePath || '//via.placeholder.com/300x200'} alt="" />).toJSON()
 expect(image).toMatchSnapshot()
 
